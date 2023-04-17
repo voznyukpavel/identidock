afc6f1942bdeccc5ccbfe76f3c33d75cb1f09ee4
pavelu <pavelu@paveluhost.paveludomain>
2023-04-17 11:00:35 +0300

    remove test prod condition

--- cmd.sh
+++ cmd.sh
@@ -7,11 +7,6 @@ if [ "$ENV" = 'DEV' ]; then
 elif [ "$ENV" = 'UNIT' ]; then 
   echo "Running Unit Tests"
   exec python "tests.py"
-elif [ "$ENV" = 'PROD_TEST' ]; then
-  echo "Running Production Server after testing"
-  exec python "tests.py"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
-             --callable app --stats 0.0.0.0:9191else
 else  
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
